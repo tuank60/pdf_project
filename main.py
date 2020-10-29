ede3b6d307d4eca66c1e579655cd7bbd1ecdee98
DoAnh Tuan <doanhtuan14111997@gmail.com>
2020-10-29 10:02:47 +0700

    12

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Sanic("hello_example")
 
 @app.route("/")
 async def test(request):
-  return json({"hello": "world solution"})
+  return json({"hello": "world solution github1"})
   # return await response.file('GFG.pdf')
   # return response.empty()
 
